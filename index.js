1156c18c0c9cb781f45960db42d552450d3dfadd
aron k koech <varokoaruna81@gmail.com>
2024-10-05 19:34:26 +0300

    creates various arrow methods

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats=["Milo","Otis","Garfield"];
+
+    function destructivelyAppendCat(Ralph) {
+        cats.push("Ralph"); 
+    }
+  function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob")
+  }
+  function destructivelyRemoveLastCat(Garfield){
+    cats.splice(-1)
+  }
+  function destructivelyRemoveFirstCat(Milo) {
+    cats.splice(0,1)
+  }
+  function appendCat(Broom) {
+    const newCats=[...cats,  "Broom"];
+    return newCats;
+  }
+  function prependCat(Arnold) {
+    const newCats=["Arnold",...cats];
+    return newCats;
+  }
+  function removeLastCat() {
+    return cats.slice(0, -1); 
+}
+
+function removeFirstCat() {
+    return cats.slice(1); 
+}
